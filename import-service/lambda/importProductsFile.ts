3465152e8084ad6467fecd139dfc016e5b078860
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-06-25 16:09:25 +0200

    feat: create new import-service cdk, api gateway and importProductsFile lambda

---
+++ import-service/lambda/importProductsFile.ts
@@ -0,0 +1,47 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+exports.handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log("request", JSON.stringify(event));
+    const headers = {
+        "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Methods": '*'
+    }
+    const fileName = event.queryStringParameters?.name;
+    console.log(fileName);
+    
+    if (!fileName) {
+        return {
+            statusCode: 400,
+            headers,
+            body: JSON.stringify({ message: "Missing required query parameter: name" })
+        };
+    }
+
+    const client = new S3Client({});
+    const command = new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: `uploaded/${fileName}`,
+    });
+
+    
+   
+    try {
+        const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+         const response = {
+            statusCode: 200,
+            headers, 
+            body: JSON.stringify({url: signedUrl})
+        };
+        return response;
+    } catch (error) {
+        console.error("Error getting object from S3:", error);
+         return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ message: "Internal Server Error" })
+        }; 
+    }
+}
---
+++ import-service/lib/import-service-stack.ts
@@ -0,0 +1,46 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class ImportServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const importBucket = s3.Bucket.fromBucketName(this, 'ImportBucket', 'import-service-bkt');
+
+    const importProductsFileFunction = new lambda.Function(this, 'importProductsFileFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'importProductsFile.handler',
+      environment: {
+        BUCKET_NAME: importBucket.bucketName,
+      },
+    })
+
+    importBucket.grantReadWrite(importProductsFileFunction);
+
+    const api = new apigateway.RestApi(this, 'import-api', {
+      restApiName: 'Import Service',
+      cloudWatchRole: true,
+    })
+
+    const importProductsFileResource = api.root.addResource('import');
+    const importProductsFileLambdaIntegration = new apigateway.LambdaIntegration(importProductsFileFunction);
+    importProductsFileResource.addMethod('GET', importProductsFileLambdaIntegration, {
+      requestParameters: {
+        'method.request.querystring.name': true
+      }
+    });
+  
+    const deployment = new apigateway.Deployment(this, 'Deployment', { api });
+
+    const devStage = new apigateway.Stage(this, 'dev-stage', {
+      stageName: 'dev',
+      deployment 
+    });
+
+    api.deploymentStage = devStage;
+  }
+}
