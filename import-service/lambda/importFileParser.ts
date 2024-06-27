641f00dbcdef392ed72fb6b12052c3488162cb78
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-06-27 09:14:40 +0200

    feat: move uploaded and parsed object to parsed folder

--- import-service/lambda/importFileParser.ts
+++ import-service/lambda/importFileParser.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { CopyObjectCommand, DeleteObjectCommand, GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { S3Event } from 'aws-lambda';
 import * as csvParser from "csv-parser";
 import { ProductWithStockType } from "../types/typeProductWithStock";
@@ -15,27 +15,56 @@ exports.handler = async (event: S3Event) => {
         console.log("Object key:", key);
 
         const client = new S3Client({});
-        const command = new GetObjectCommand({
-            Bucket: bucket,
-            Key: key
-        });
 
         try {
-            const result = await client.send(command);
-        
+            const getCommand = new GetObjectCommand({
+                Bucket: bucket,
+                Key: key
+            });
+            const result = await client.send(getCommand);
+            
             const productsFromCSV: ProductWithStockType[] = [];
-        
-            if (result.Body instanceof Readable) {
-                result.Body
-                    .pipe(csvParser({ separator: ';' }))
-                    .on('data', (data) => productsFromCSV.push(data))
-                    .on('end', () => console.log(productsFromCSV))
-                    .on('error', (error) => {
-                        console.error('Error parsing CSV:', error);
-                    });;
-            } else {
-                console.log('result is not a Readable stream.');
+            
+            if (!result.Body) {
+                throw new Error('Body is undefined');
             }
+            const readableStream = result.Body as Readable;
+            
+            await new Promise<void>((resolve, reject) => {
+                readableStream
+                .pipe(csvParser({ separator: ';' }))
+                .on('data', (data) => productsFromCSV.push(data))
+                .on('end', async () => {
+                    console.log(productsFromCSV);
+                    resolve();
+                })
+                .on('error', (error) => {
+                    console.error('Error parsing CSV:', error);
+                    reject(error);
+                });
+            });
+            
+            const newKey = key.replace('uploaded', 'parsed')
+            console.log("newKey:", newKey);
+
+            const copyCommand = new CopyObjectCommand({
+                Bucket: bucket,
+                CopySource: `${bucket}/${key}`,
+                Key: newKey
+            })
+            
+            const copyResponse = await client.send(copyCommand);
+            console.log("File copied from uploaded to parsed:", copyResponse);
+        
+            const deleteCommand = new DeleteObjectCommand({
+                Bucket: bucket,
+                Key: key
+            })
+            
+            await client.send(deleteCommand);
+            console.log('File deleted from uploaded:');
+            console.log(`Object ${key} was succesfully moved to parsed folder`);
+            
         } catch (error) {
             console.error('Error getting object from S3:', error);
         }
