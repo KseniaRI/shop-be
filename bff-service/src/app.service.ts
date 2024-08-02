67077ed2864622d0a391911a00126463ae2baac3
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-08-02 16:46:06 +0200

    feat:create Nest FE-requests handler

---
+++ bff-service/src/app.service.ts
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { Request } from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Injectable()
+export class AppService {
+  getServiceURL(originalURL: string): string | undefined {
+    const recipient = originalURL.split('/')[1];
+    const recipirentUrl = process.env[recipient];
+    console.log('recipirentUrl', recipirentUrl);
+    return recipirentUrl;
+  }
+  async forwardRequest(req: Request) {
+    const { method, originalUrl, body } = req;
+
+    console.log('method', method);
+    console.log('originalUrl', originalUrl);
+    console.log('body', body);
+    const recipirentUrl = this.getServiceURL(originalUrl);
+    const hasBody = Object.keys(req.body || {}).length > 0;
+
+    if (recipirentUrl) {
+      const axiosConfig = {
+        method,
+        url: `${recipirentUrl}${originalUrl}`,
+        ...(hasBody && { data: body }),
+      };
+
+      console.log('axiosConfig', axiosConfig);
+
+      return axios(axiosConfig)
+        .then((response) => response.data)
+        .catch((error) => {
+          if (error.response) {
+            const { status, data } = error.response;
+            throw { status, data };
+          } else {
+            throw { status: 500, data: { error: error.message } };
+          }
+        });
+    }
+  }
+}
